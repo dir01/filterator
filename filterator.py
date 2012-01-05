5de038d565bb2cfb904aa51e3e62738d91c719c9
Andrew Zhu <dir01@dir01.org>
2012-01-05 17:35:15 +0400

    Changed tested objects to people

--- filterator.py
+++ filterator.py
@@ -36,66 +36,35 @@ class Filterable(object):
         return filtering_function
 
 
-Planet = namedtuple('Planet', 'name aphelion perihelion mass satellites')
-
-
 class FilteratorTestCase(unittest2.TestCase):
     def setUp(self):
-        self.mercury = Planet(name='Mercury',
-            aphelion=69816900, perihelion=46001200, mass=Decimal('3.3E+23'),
-            satellites=[]
-        )
-        self.venus = Planet(name='Venus',
-            aphelion=108942109, perihelion=107476259, mass=Decimal('4.8E+24'),
-            satellites=[]
-        )
-        self.earth = Planet(name='Earth',
-            aphelion=1152098232, perihelion=147098290, mass=Decimal('5.9E+24'),
-            satellites=['Moon']
-        )
-        self.mars = Planet(name='Mars',
-            aphelion=249209300, perihelion=206669000, mass=Decimal('6.4E+23'),
-            satellites=['Phobos', 'Deimos']
-        )
-        self.jupiter = Planet(name='Jupiter',
-            aphelion=816520800, perihelion=740573600, mass=Decimal('1.8E+27'),
-            satellites=['Io', 'Europa', 'Ganymede', 'Callisto']
-        )
-        self.saturn = Planet(name='Saturn',
-            aphelion=1513325783, perihelion=1353572956, mass=Decimal('5.6E+26'),
-            satellites=['Mimas', 'Enceladus', 'Tethys', 'Dione', 'Rhea', 'Titan', 'Iapetus']
-        )
-        self.uranus = Planet(name='Uranus',
-            aphelion=3004419704, perihelion=2748938461, mass=Decimal('8.6E+25'),
-            satellites=['Puck', 'Miranda', 'Ariel', 'Umbriel', 'Titania', 'Oberon']
-        )
-        self.neptune = Planet(name='Neptune',
-            aphelion=4553946490, perihelion=4452940833, mass=Decimal('1.0E+26'),
-            satellites=['Naiad', 'Thalassa','Despina', 'Galatea', 'Larissa', 'Proteus']
-        )
-        self.planets = Filterable([
-            self.mercury,
-            self.venus,
-            self.earth,
-            self.mars,
-            self.jupiter,
-            self.saturn,
-            self.uranus,
-            self.neptune
+        Person = namedtuple('Person', 'name age sex children')
+        self.marta = Person('Marta', 2, 'F', [])
+        self.joe = Person('Joe', 7, 'M', [])
+        self.alice = Person('Alice', 23, 'F', [self.marta])
+        self.bob = Person('Bob', 31, 'M', [self.marta, self.joe])
+        self.people = Filterable([
+            self.marta,
+            self.joe,
+            self.alice,
+            self.bob,
         ])
 
 
 class TestFilter(FilteratorTestCase):
     def test_filter_by_string(self):
-        self.assertEqual([self.mars], self.planets.filter(name='Mars').iterable)
+        self.assertEqual([self.bob], self.people.filter(name='Bob'))
+
+    def test_filter_by_int(self):
+        self.assertEqual([self.alice], self.people.filter(age=23))
 
 
 class TestGet(FilteratorTestCase):
     def test_get_one(self):
-        self.assertEqual(self.mars, self.planets.filter(name='Mars').get())
+        self.assertEqual(self.bob, self.people.filter(name='Bob').get())
 
     def test_get_with_constrains(self):
-        self.assertEqual(self.mars, self.planets.get(name='Mars'))
+        self.assertEqual(self.bob, self.people.get(name='Bob'))
 
 
 
