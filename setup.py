1225ff19f47416a195bcff1f0bd0f36326fb4d22
Andrew Zhu <dir01@dir01.org>
2012-02-15 20:04:08 +0400

    setup.py via setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
+!/usr/bin/env python
 
-from distutils.core import setup
+from __future__ import with_statement
 
-setup(name='Filterator',
-    version='0.8',
-    description='Utility for chainable operations upon collections',
-    author='Andrey Zhu',
-    author_email='dir01@dir01.org',
-    url='https://github.com/dir01/filterator',
-    packages=['filterator'],
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name = 'filterator',
+    version = '0.01',
+    description = 'Utility for chainable operations upon collections',
+    author = 'Andrey Zhu',
+    author_email = 'dir01@dir01.org',
+    url = 'https://github.com/dir01/filterator',
+    packages = find_packages(exclude=['tests', 'tests.*']),
 )
+
