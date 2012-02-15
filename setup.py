930409792d5418e532553c846e1b3bb19c78e06e
Andrew Zhu <dir01@dir01.org>
2012-02-15 19:36:05 +0400

    + setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Filterator',
+    version='0.8',
+    description='Utility for chainable operations upon collections',
+    author='Andrey Zhu',
+    author_email='dir01@dir01.org',
+    url='https://github.com/dir01/filterator',
+    packages=['filterator'],
+)
